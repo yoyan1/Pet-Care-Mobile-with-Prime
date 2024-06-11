d72a918838028e54c898b94b5b77fe6062fc0913
yoyan1 <yoyan947@gmail.com>
2024-06-11 11:55:24 -0600

    added file structuring

---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+<template>
+    <div class="container">
+        <!-- <img src="../components/images/logo.jpg" class="logo" width="100px" alt=""> -->
+        <h1>WELCOME</h1>
+        <div class="btn">
+          <RouterLink to='login' class="link">Get Started </RouterLink>
+          <RouterLink to='scanqr' class="link qr">Scan QR </RouterLink>
+        </div>
+        <div class="image-bg">
+          <div class="bg"></div>
+          <!-- <img src="../components/images/dog.jpg" alt=""> -->
+        </div>
+    </div>
+</template>
